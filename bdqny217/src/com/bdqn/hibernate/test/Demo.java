d9c31e09e38cbb0b61093a90b27c996b5495ade1
lzybj <lzybj@icloud.com>
2013-10-05 11:32:26 +0800

    hibernate案例1

---
+++ bdqny217/src/com/bdqn/hibernate/test/Demo.java
@@ -0,0 +1,28 @@
+package com.bdqn.hibernate.test;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.bdqn.hibernate.domain.HosType;
+import com.bdqn.hibernate.factory.HibernateSessionFactory;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Session session = HibernateSessionFactory.getSession();
+		String hsql = "from HosType";
+		Query query = session.createQuery(hsql);
+		List<HosType> data = query.list();
+		for (HosType hosType : data) {
+			System.out.println(hosType.getHtname());
+		}
+		HibernateSessionFactory.closeSession();
+	}
+
+}

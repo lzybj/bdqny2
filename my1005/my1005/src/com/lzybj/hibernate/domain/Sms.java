a96dd6d452a0da67761a8996af383ea7798de984
lzybj <lzybj@icloud.com>
2013-10-05 19:09:44 +0800

    Struts2框架之DOJO标签

---
+++ my1005/my1005/src/com/lzybj/hibernate/domain/Sms.java
@@ -0,0 +1,95 @@
+package com.lzybj.hibernate.domain;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * Sms entity. @author MyEclipse Persistence Tools
+ */
+
+@Entity
+public class Sms implements java.io.Serializable {
+
+	// Fields
+
+	@Id
+	@GeneratedValue
+	private Integer smsId;
+	@ManyToOne
+	private SmsUsers smsUsersBySendUsid;
+	@ManyToOne
+	private SmsUsers smsUsersByResvUsid;
+	private String smsContent;
+	private Integer smsState;
+	private Date smsDate;
+
+	// Constructors
+
+	/** default constructor */
+	public Sms() {
+	}
+
+	/** full constructor */
+	public Sms(SmsUsers smsUsersBySendUsid, SmsUsers smsUsersByResvUsid,
+			String smsContent, Integer smsState, Date smsDate) {
+		this.smsUsersBySendUsid = smsUsersBySendUsid;
+		this.smsUsersByResvUsid = smsUsersByResvUsid;
+		this.smsContent = smsContent;
+		this.smsState = smsState;
+		this.smsDate = smsDate;
+	}
+
+	// Property accessors
+
+	public Integer getSmsId() {
+		return this.smsId;
+	}
+
+	public void setSmsId(Integer smsId) {
+		this.smsId = smsId;
+	}
+
+	public SmsUsers getSmsUsersBySendUsid() {
+		return this.smsUsersBySendUsid;
+	}
+
+	public void setSmsUsersBySendUsid(SmsUsers smsUsersBySendUsid) {
+		this.smsUsersBySendUsid = smsUsersBySendUsid;
+	}
+
+	public SmsUsers getSmsUsersByResvUsid() {
+		return this.smsUsersByResvUsid;
+	}
+
+	public void setSmsUsersByResvUsid(SmsUsers smsUsersByResvUsid) {
+		this.smsUsersByResvUsid = smsUsersByResvUsid;
+	}
+
+	public String getSmsContent() {
+		return this.smsContent;
+	}
+
+	public void setSmsContent(String smsContent) {
+		this.smsContent = smsContent;
+	}
+
+	public Integer getSmsState() {
+		return this.smsState;
+	}
+
+	public void setSmsState(Integer smsState) {
+		this.smsState = smsState;
+	}
+
+	public Date getSmsDate() {
+		return this.smsDate;
+	}
+
+	public void setSmsDate(Date smsDate) {
+		this.smsDate = smsDate;
+	}
+
+}

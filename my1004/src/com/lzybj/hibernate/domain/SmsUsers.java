a05fece0cc2831a48297b68b81b7923b8be072ea
lzybj <lzybj@icloud.com>
2013-10-05 11:48:26 +0800

    Struts2框架之DOJO标签

---
+++ my1004/src/com/lzybj/hibernate/domain/SmsUsers.java
@@ -0,0 +1,77 @@
+package com.lzybj.hibernate.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * SmsUsers entity. @author MyEclipse Persistence Tools
+ */
+
+public class SmsUsers implements java.io.Serializable {
+
+	// Fields
+
+	private Integer usid;
+	private String uname;
+	private String upwd;
+	private Set smsesForSendUsid = new HashSet(0);
+	private Set smsesForResvUsid = new HashSet(0);
+
+	// Constructors
+
+	/** default constructor */
+	public SmsUsers() {
+	}
+
+	/** full constructor */
+	public SmsUsers(String uname, String upwd, Set smsesForSendUsid,
+			Set smsesForResvUsid) {
+		this.uname = uname;
+		this.upwd = upwd;
+		this.smsesForSendUsid = smsesForSendUsid;
+		this.smsesForResvUsid = smsesForResvUsid;
+	}
+
+	// Property accessors
+
+	public Integer getUsid() {
+		return this.usid;
+	}
+
+	public void setUsid(Integer usid) {
+		this.usid = usid;
+	}
+
+	public String getUname() {
+		return this.uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUpwd() {
+		return this.upwd;
+	}
+
+	public void setUpwd(String upwd) {
+		this.upwd = upwd;
+	}
+
+	public Set getSmsesForSendUsid() {
+		return this.smsesForSendUsid;
+	}
+
+	public void setSmsesForSendUsid(Set smsesForSendUsid) {
+		this.smsesForSendUsid = smsesForSendUsid;
+	}
+
+	public Set getSmsesForResvUsid() {
+		return this.smsesForResvUsid;
+	}
+
+	public void setSmsesForResvUsid(Set smsesForResvUsid) {
+		this.smsesForResvUsid = smsesForResvUsid;
+	}
+
+}
